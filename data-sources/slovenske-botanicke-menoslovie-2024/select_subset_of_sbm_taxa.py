cdfc3ea49b99eada861b4d50400db6782ea8add7
samsucik <ssucik@cisco.com>
2025-12-16 18:14:31 +0100

    f extract sbm

---
+++ data-sources/slovenske-botanicke-menoslovie-2024/select_subset_of_sbm_taxa.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from tqdm import tqdm
+
+select_by_file_name = "missing_taxa"
+
+all_sbm_taxa = pd.read_csv("taxa.csv")
+taxa_to_select = pd.read_csv(f"{select_by_file_name}.csv")["sciName"].values.tolist()
+
+def does_row_match(row, sci_name):
+    return sci_name == row[f"{row['rank']}_scientific"]
+
+
+def select_sbm_taxa(all_taxa, sci_names_to_select):
+    selected_rows = []
+    for sci_name in tqdm(sci_names_to_select):
+        rows = all_taxa[all_taxa.apply(lambda row: does_row_match(row, sci_name), axis=1)]
+        if len(rows) != 1:
+            print(f"Found {len(rows)} for {sci_name}")
+        else:
+            selected_rows.append(rows.iloc[0].to_dict())
+    return pd.DataFrame(selected_rows)
+
+
+selected_sbm_taxa = select_sbm_taxa(all_sbm_taxa, taxa_to_select)
+
+selected_sbm_taxa.to_json(f"taxa_selected_by_{select_by_file_name}.json", orient="records")
